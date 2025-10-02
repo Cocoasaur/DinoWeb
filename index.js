220305b0c55a39827b859131e9341d744ed40ba9
JL <cocoasaurjl@gmail.com>
2025-10-02 08:06:04 +0800

    Remove old design (HTML, CSS, JavaScript) for redesign.

--- index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburger = document.getElementById('hamburgerBtn');
-    const mobileNav = document.querySelector('.mobile-only');
-
-    hamburger.addEventListener('click', function () {
-        mobileNav.classList.toggle('show');
-        hamburger.classList.toggle('active');
-    });
-});
