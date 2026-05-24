05a5054431b67694a83e07b3df48a057420d4952
JL <cocoasaurjl@gmail.com>
2026-05-25 00:09:07 +0800

    add GitHub Pages workflow and base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/DinowWeb/',
   plugins: [react()],
   build: {
     target: 'es2020',
@@ -14,10 +15,8 @@ export default defineConfig({
         }
       }
     },
-    // Reduce chunk size warnings
     chunkSizeWarningLimit: 600,
   },
-  // Optimize deps for faster dev and better tree-shaking
   optimizeDeps: {
     include: ['three', '@react-three/fiber', '@react-three/drei'],
   }
